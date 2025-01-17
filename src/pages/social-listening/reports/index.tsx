3ecbae3cc8e77ae7ad12a9447001e3f27757cba2
ZAKMOUCHTATI44 <155732574+ZAKMOUCHTATI44@users.noreply.github.com>
2025-01-17 12:09:10 +0100

    verified

--- src/pages/social-listening/reports/index.tsx
+++ src/pages/social-listening/reports/index.tsx
@@ -13,6 +13,7 @@ const Page = () => {
       {data && (
         <Grid container spacing={6}>
           <Grid item xs={12} md={6} lg={6}>
+            HELLO WORLD
             <CardDetails influencer={data} />
           </Grid>
           <InfluenceScore data={data} />
